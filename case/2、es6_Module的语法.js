58963d04184faf0c9ba31bd7c0163e525f84c3d6
luocg <306139142@qq.com>
2019-01-23 17:02:36 +0800

    20190123新加
    20190123新加

--- case/2、es6_Module的语法.js
+++ case/2、es6_Module的语法.js
@@ -1,18 +1,18 @@
-// JavaScript 数据类型
-// 字符串（String）、数字(Number)、布尔(Boolean)、数组(Array)、对象(Object)、空（Null）、未定义（Undefined）
+// Module 语法
 
-// JavaScript 对象
-// 对象由花括号分隔。在括号内部，对象的属性以名称和值对的形式 (name : value) 来定义。属性由逗号分隔：
-// var person={firstname:"John", lastname:"Doe", id:5566};
-// 上面例子中的对象 (person) 有三个属性：firstname、lastname 以及 id。
-// 空格和折行无关紧要。声明可横跨多行：
-// var person={
-// firstname : "John",
-// lastname  : "Doe",
-// id        :  5566
-// };
-// 对象属性有两种寻址方式：
-// 实例
-// name=person.lastname;
-// name=person["lastname"];
+// 1.概述
+// 2.严格模式
+// 3.export 命令
+// 4.import 命令
+// 5.模块的整体加载
+// 6.export default 命令
+// 7.export 与 import 的复合写法
+// 8.模块的继承
+// 9.跨模块常量
+// 10.import()
 
+let x = 1;
+let y = 2;
+[x, y] = [y, x];
+console.log(x);
+console.log(y);
